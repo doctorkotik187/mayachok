b7e1387e120a598c96a17d2e927854b264323153
doctorkotik187 <217476020+doctorkotik187@users.noreply.github.com>
2026-06-23 10:46:24 +0900

    add csp

--- src/clj/mayachok/mayachok/web/middleware/core.clj
+++ src/clj/mayachok/mayachok/web/middleware/core.clj
@@ -4,10 +4,25 @@
    [ring.middleware.defaults :as defaults]
    [ring.middleware.session.cookie :as cookie]))
 
+(def ^:private csp-header
+  (str "default-src 'self'; "
+       "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; "
+       "script-src 'self' 'unsafe-inline'; "
+       "img-src 'self' data:; "
+       "font-src 'self' https://cdn.jsdelivr.net; "
+       "connect-src 'self'; "
+       "frame-ancestors 'none'"))
+
+(defn- wrap-csp [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (assoc-in response [:headers "Content-Security-Policy"] csp-header))))
+
 (defn wrap-base
   [{:keys [metrics site-defaults-config cookie-secret] :as opts}]
   (let [cookie-store (cookie/cookie-store {:key (.getBytes ^String cookie-secret)})]
     (fn [handler]
       (cond-> ((:middleware env/defaults) handler opts)
         true (defaults/wrap-defaults
-              (assoc-in site-defaults-config [:session :store] cookie-store))))))
+              (assoc-in site-defaults-config [:session :store] cookie-store))
+        true wrap-csp))))
